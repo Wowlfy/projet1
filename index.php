388d1c95111f62b3876f1c97740d8a83a60e8f6d
jaldabaoth-code <grialazurabi@gmail.com>
2021-03-16 08:44:59 +0100

    cest juste le test avec le squelette du site

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Le CV de John McClane">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styletimeline.css">
+    <title>John McClane</title>
+</head>
+<body>
+  <?php
+    include("nav.php");
+  ?>
+  <h1>John McClane</h1>
+  <hr>
+  <h2>Lieutenant de Police</h2>
+  <hr>
+  <h3>Présentation</h3>
+  <hr>
+  <h3>Compétences</h3>
+  <hr>
+  <h3>Expériences</h3>
+  <hr>
+  <h3>Formation</h3>
+  <hr>
+  <h3>Centre d'intérêt</h3>
+  <hr>
+  <h3>Me Contacter</h3>
+
+  <?php
+    include("formulaire.php");
+  ?>
+  <script src="jstimeline.js"></script>
+</body>
+</html>
