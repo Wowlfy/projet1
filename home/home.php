4cd02ba13254a720e805520b9bb712adb8ae5ef9
jaldabaoth-code <grialazurabi@gmail.com>
2021-03-17 15:46:13 +0100

    debut du code

---
+++ home/home.php
@@ -0,0 +1,10 @@
+<link href="css/style.css" rel="stylesheet">
+
+<section class="home">
+    <h1>John McClane</h1>
+    <h2>Lieutenant de Police</h2>
+    <div class="images">
+        <img src="img/nakatomi_tower.png" alt="nakatomi_tower" class="cover">
+    </div>
+        <img src="img/john_mccane.jpg" alt="John McClane" class="photo">
+</section>
