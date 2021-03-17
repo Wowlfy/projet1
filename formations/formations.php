19e1b9ef9c3e0f5f5af5072978ba46d11be74bda
JuMn88 <moineau.julien@hotmail.fr>
2021-03-17 15:46:45 +0100

    initialisation des fichiers formations

---
+++ formations/formations.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formations</title>
+    <link href="css/formations_styles.css" rel="stylesheet">
+</head>
+
+<body>
+<h2>Formations</h2>
+
+<div class="cards">
+    <div class="card">
+        <div class="card-header">
+            <div class="card-img"></div>
+        </div>
+            <div class="card-body">
+                <h3 class="card-title">High school</h3>
+                <p>High school short description</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="cards">
+    <div class="card">
+        <div class="card-header">
+            <div class="card-img"></div>
+        </div>
+        <div class="card-body">
+            <h3 class="card-title">Police school</h3>
+            <p>Police school short description</p>
+        </div>
+    </div>
+</div>
+<div class="cards">
+    <div class="card">
+        <div class="card-header">
+            <div class="card-img"></div>
+        </div>
+            <div class="card-body">
+                <h3 class="card-title">Shooting</h3>
+                <p>John McClane at a shooting range</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="cards">
+    <div class="card">
+        <div class="card-header">
+            <div class="card-img"></div>
+        </div>
+            <div class="card-body">
+                <h3 class="card-title">Piloting</h3>
+                <p>John McClane in front of an helicopter</p>
+            </div>
+        </div>
+    </div>
+</div>     
+</body>
+</html>
