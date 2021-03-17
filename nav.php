f25de79cbe77f5aab11c9ef48763b7c1b8e2e9cb
glochlainn <lochlainn.gadault@gmail.com>
2021-03-17 12:00:02 +0100

    Initialisation des default folders et du fichier main.css / structuration initiale du repository v2

--- nav.php
+++
@@ -1,9 +0,0 @@
- <nav class="navbar">
-   <a href="#presentation">Présentation</a>
-   <a href="#skills">Compétences</a>
-   <a href="#experience">Expériences</a>
-   <a href="#training">Formation</a>
-   <a href="interest">Centre d'intérêt</a>
-   <a href="contact">Me Contacter</a>
-   <a href="burgerMenu">BURGER</a>
- </nav>
