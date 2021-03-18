f0712e2ff119892930b5debc60724cc00970bdc2
jaldabaoth-code <grialazurabi@gmail.com>
2021-03-18 01:21:37 +0100

    modification du fichier home.php

---
+++ home.php
@@ -0,0 +1,43 @@
+<link href="assets/home_style.css" rel="stylesheet">
+
+<section class="home">
+
+    <div class="heading">
+        <div class="mainHeading">
+            <h1>Hello I'm John McClane</h1>
+            <h2>Lieutenant de la Police de New York</h2>  
+        </div>
+        
+        <div class="bioHeading">
+
+            <img class="photo" src="assets/img/john_mcclane.png" alt="John McCane">
+            <div class="bioText">
+                <h3>ABout Me</h3>
+                <p>
+                    Jeunesse
+                    John McClane, Sr. est né le 19 mars 1955 à Plainfield, au New Jersey, au
+                    x Etats-Unis. Son père était un policier irlando-américain alors que sa mère ét
+                    ait une femme de ménage. Au cours de sa jeunesse, John a malheureusement perdu son
+                    père et lorsqu'il est devenu adulte, sa mère a elle aussi perdu la vie. McClane est
+                    ensuite allé vivre dans la ville de New York où il a rejoint la police locale. Il a
+                    très vite rencontré un certain succès et cela lui a donc valu d'être promu au grade de
+                    lieutenant par son capitaine.
+                    À une date inconnue, McClane a également rencontré et épousé une femme nommée Holly Gennero 
+                    avec qui il a deux enfants, John "Jack" et Lucy. Holly est finalement partie à Los Angeles pour 
+                    poursuivre sa carrière de femme d'affaires (que John pense sans issue).
+                    Piège de cristal
+                    Nous sommes à l'époque des fêtes de fin d'année 1988 et cela fait onze ans que John McClane 
+                    exerce sa profession de policier. À l'occasion des fêtes, il décide d'aller rendre visite à Holly Gennero
+                    sur son lieu de travail, le Nakatomi Plaza, et passer le réveillon en compagnie de ses enfants. Il emprunte 
+                    un avion pour voyager et discute brièvement avec le passager qui est assis à côté de lui. Une fois 
+                    à l'aéroport, il rencontre son chauffeur, un certain Argyle, qui le mène en limousine jusqu'au 
+                    Nakatomi Plaza. Se rendant au trentième étage, là où se trouve le bureau d'Holly, McClane rencontre son 
+                    patron, Joe Takagi, qui le mène jusqu'à sa femme. Le policier américain est également présenté à 
+                    un homme d'affaires nommé Harry Ellis qui dit avoir beaucoup entendu parler de lui. John est ensuite conduit
+                    par Holly jusqu'à la salle de bain du bureau d'Ellis.
+                </p>
+                <a href="#"><img class="arrow" src="assets/img/arrow.png" alt="fleche"></a>
+            </div>
+        </div>
+    </div>
+</section>
