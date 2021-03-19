1d3b1bfcdb29cd7a54c83ca2836b89ddd8b75859
glochlainn <lochlainn.gadault@gmail.com>
2021-03-19 13:29:30 +0100

    zzzzz fix

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <!-- <link rel="stylesheet" href="assets/main.css"> -->
+    <link rel="stylesheet" href="assets/navbar_style.css">
     <title>John McClane</title>
 </head>
 
@@ -14,12 +15,12 @@
 
 <?php 
 
-require_once 'home.php';
+/*require_once 'home.php';
 require_once 'skills.php';
 require_once 'experiences.php';
 require_once 'formations.php';
-/* require_once 'interests.php'; */
-require_once 'form.php';
+require_once 'interests.php'; 
+require_once 'form.php'; */
 
 ?> 
 
