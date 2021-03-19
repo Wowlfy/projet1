ac897beaca5da142ddc052597f11ceeb19fddc15
jaldabaoth-code <grialazurabi@gmail.com>
2021-03-19 01:36:23 +0100

    ajustement pour push vendredi

--- assets/experiences_javascript.js
+++ assets/experiences_javascript.js
@@ -17,22 +17,3 @@ var timelineSwiper = new Swiper ('.timeline .swiper-container', {
     }
   }
 });
-
-
-
-
-
-
-
-
-
-/*fonctionel*/
-
-/*var timelineSwiper = new Swiper ('.timeline .swiper-container', {
-    direction: 'vertical',
-    speed: 1600,
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-  });*/
--- experiences.php
+++ experiences.php
@@ -1,52 +1,54 @@
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/css/swiper.min.css'>
-<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800'><link rel="stylesheet" href="assets/experiences_style.css">
-
-
-
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800'>
+<link rel="stylesheet" href="assets/experiences_style.css">
 
 <section class="body_experiences">
+  <div class="container_experiences">
+    <h1 class="title_experiences">Experiences</h1>
+    <div class="timeline">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide" style="background-image: url(assets/img/experiences_1.jpg" data-year="1988">
+            <div class="swiper-slide-content">
+              <span class="timeline-year">1988</span>
+              <h4 class="timeline-title">Nakatomi Plaza</h4>
+              <p class="timeline-text">Die Hard 1</p>
+            </div>
+          </div>
+          <div class="swiper-slide" style="background-image: url(assets/img/experiences_2.jpg" data-year="1990">
+            <div class="swiper-slide-content"><span class="timeline-year">1990</span>
+              <h4 class="timeline-title">Aeroport</h4>
+              <p class="timeline-text">Die Hard 2</p>
+            </div>
+          </div>
+          <div class="swiper-slide" style="background-image: url(assets/img/experiences_3.jpg" data-year="1990">
+            <div class="swiper-slide-content"><span class="timeline-year">1990</span>
+              <h4 class="timeline-title">Aeroport</h4>
+              <p class="timeline-text">Die Hard 2</p>
+            </div>
+          </div>
+          <div class="swiper-slide" style="background-image: url(assets/img/experiences_4.jpg" data-year="2007">
+            <div class="swiper-slide-content"><span class="timeline-year">2007</span>
+              <h4 class="timeline-title">Reseau Informatique</h4>
+              <p class="timeline-text">Die Hard 4</p>
+            </div>
+          </div>
+          <div class="swiper-slide" style="background-image: url(assets/img/experiences_5.jpg" data-year="2007">
+            <div class="swiper-slide-content"><span class="timeline-year">2007</span>
+              <h4 class="timeline-title">Reseau Informatique</h4>
+              <p class="timeline-text">Die Hard 4</p>
+            </div>
+          </div>
 
-<div class="container_experiences">
-  <h1 class="title_experiences">Experiences</h1>
-  <div class="timeline">
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide" style="background-image: url(assets/img/die_hard1.jpg" data-year="1988">
-          <div class="swiper-slide-content">
-            <span class="timeline-year">1988</span>
-            <h4 class="timeline-title">Nakatomi Plaza</h4>
-            <p class="timeline-text">Die Hard 1</p>
-          </div>
         </div>
-        <div class="swiper-slide" style="background-image: url(assets/img/die_hard2.jpg" data-year="1989">
-          <div class="swiper-slide-content"><span class="timeline-year">1989</span>
-            <h4 class="timeline-title">Aeroport</h4>
-            <p class="timeline-text">Die Hard 2</p>
-          </div>
-        </div>
-        <div class="swiper-slide" style="background-image: url(assets/img/die_hard3.jpg" data-year="1992">
-          <div class="swiper-slide-content"><span class="timeline-year">1992</span>
-            <h4 class="timeline-title">Banque</h4>
-            <p class="timeline-text">Die Hard 3</p>
-          </div>
-        </div>
-        <div class="swiper-slide" style="background-image: url(asserts/img/die_hard2.jpg" data-year="2007">
-          <div class="swiper-slide-content"><span class="timeline-year">2007</span>
-            <h4 class="timeline-title">Reseau Informatique</h4>
-            <p class="timeline-text">Die Hard 4</p>
-          </div>
-        </div>
-
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-pagination"></div>
       </div>
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
-      <div class="swiper-pagination"></div>
     </div>
   </div>
-</div>
-<!-- partial -->
+  <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js'></script><script  src="assets/experiences_javascript.js"></script>
 
 </section>
