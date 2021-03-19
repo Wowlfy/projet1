030a1a1fa52736c77f5b31a65662f4a74d06c420
Wowlfy <maxime.giraudeau@hotmail.fr>
2021-03-19 10:19:13 +0100

    did the form part

--- form.php
+++ form.php
@@ -5,25 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>john McClane</title>
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="assets/form_style.css">
 </head>
 
 <body>
-    <h1>Contact moi</h1>
-    <form>
-        <input type="text" placeholder="Nom" id="lastName" />
-        <br><br>
-        <input type="text" placeholder="Prenom" id="firstName" />
-        <br><br>
-        <input type="email" placeholder="Email" id="email" />
-        <br><br>
-        <label>Commentaire</label>
-        <br>
-        <textarea id="comments" rows="4"></textarea>
-        <br><br>
-        <button onclick="sendContact();">envoyer</button>
-    </form>
+    <section class=form>
+        <div class=head>
+            <br>
+            <h1>CONTACT MOI</h1>
+        </div>
+        <div class=formText>
+            <input type="text" style="width:300px" placeholder="Nom" id="lastName" />
+            <br><br>
+            <input type="text" style="width:300px" placeholder="Prenom" id="firstName" />
+            <br><br>
+            <input type="email" style="width:300px" placeholder="Email" id="email" />
+            <br><br>
+            <label>Commentaire</label>
+            <br>
+            <textarea id="comments" style="width:300px" rows="4"></textarea>
+            <br><br>
+        </div>
+        <div class="wrapper">
+        <a href="#" class="button">Envoyer</a>
+     
+            <br><br>
+        </div>
+    </section>
 </body>
 
 </html>
