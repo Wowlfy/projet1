a3bbbe4f36fa7ed69fb69b674bb92426052e6740
RaphaelBS-WCS <raphaelbilletservoin@gmail.com>
2021-03-23 16:08:30 +0100

    pull last update and add border-radius to .gauge

--- skills.php
+++ skills.php
@@ -6,44 +6,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Skills</title>
-    <link rel="Stylesheet" href="skills.css">
+    <link rel="Stylesheet" href="assets/skills_style.css">
+
 </head>
 
-<div class="skills-main">
-
-    <h1>SKILLS</h1>
-    <section id="skills" class="skills">
-        <div class='gauge'>
-            <div class="gauge__body">
-                <div class="gauge__fill" id="one"></div>
-                <div class="gauge__cover">50%</div>
-            </div>
-            <div class="gauge__legend">Stealth</div>
+<h1>SKILLS</h1>
+<section class="skills">
+    <div class='gauge'>
+        <div class="gauge__body">
+            <div class="gauge__fill" id="one"></div>
+            <div class="gauge__cover" data-value="50">50%</div>
         </div>
-        <div class='gauge'>
-            <div class="gauge__body">
-                <div class="gauge__fill" id="two"></div>
-                <div class="gauge__cover">25%</div>
-            </div>
-            <div class="gauge__legend">Diplomacy</div>
+        <div class="gauge__legend">Stealth</div>
+    </div>
+    <div class='gauge'>
+        <div class="gauge__body">
+            <div class="gauge__fill" id="two"></div>
+            <div class="gauge__cover" data-value="25">25%</div>
         </div>
-        <div class='gauge'>
-            <div class="gauge__body">
-                <div class="gauge__fill" id="three"></div>
-                <div class="gauge__cover">75%</div>
-            </div>
-            <div class="gauge__legend">Explosives</div>
+        <div class="gauge__legend">Diplomacy</div>
+    </div>
+    <div class='gauge'>
+        <div class="gauge__body">
+            <div class="gauge__fill" id="three"></div>
+            <div class="gauge__cover" data-value="75">75%</div>
         </div>
-        <div class='gauge'>
-            <div class="gauge__body">
-                <div class="gauge__fill" id="four"></div>
-                <div class="gauge__cover">90%</div>
-            </div>
-            <div class="gauge__legend">Charisma</div>
+        <div class="gauge__legend">Explosives</div>
+    </div>
+    <div class='gauge'>
+        <div class="gauge__body">
+            <div class="gauge__fill" id="four"></div>
+            <div class="gauge__cover" data-value="90">90%</div>
         </div>
-    </section>
-
-</div>
-
+        <div class="gauge__legend">Charisma</div>
+    </div>
+</section>
 
 </html>
