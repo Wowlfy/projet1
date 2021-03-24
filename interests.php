a661d7e0f117a6026edf0ca87c88a564e3bd0353
glochlainn <lochlainn.gadault@gmail.com>
2021-03-24 11:47:27 +0100

    interests final v2

--- interests.php
+++ interests.php
@@ -18,26 +18,26 @@
     <div class="interests-main">
 
         <div class="container">
-            <img src="https://picsum.photos/1000/300" alt="Avatar" class="image">
+            <img src="https://staticr1.blastingcdn.com/media/photogallery/2018/8/4/660x290/b_1200x680/bruce-willis-is-back-for-another-die-hard-but-this-time-it-also-features-a-young-john-mcclane-image-credit-nerdist-news-youtube_2069183.jpg" alt="etremes-sports" class="image">
             <div class="overlay">
-                <div class="text">Hello World</div>
-                <p>I'm a funny guy</p>
+                <div class="text">Extremes sports</div>
+                <p>I practice climbing without equipment and I like it</p>
             </div>
         </div>
 
         <div class="container">
-            <img src="https://picsum.photos/1000/300" alt="Avatar" class="image">
+            <img src="https://media.vanityfair.com/photos/5b48c4174da5840c0f8c07d0/3:2/w_1998,h_1332,c_limit/die-hard-anniversary-lede.jpg" alt="shooting-guy" class="image">
             <div class="overlay">
-                <div class="text">Hello Magic</div>
-                <p>I'm a funny guy</p>
+                <div class="text">Shooting</div>
+                <p>I like shooting some bad boys</p>
             </div>
         </div>
 
         <div class="container">
-            <img src="https://picsum.photos/1000/300" alt="Avatar" class="image">
+            <img src="https://i.ytimg.com/vi/zxeBOM9mDYY/maxresdefault.jpg" alt="captain-kangaroo" class="image">
             <div class="overlay">
-                <div class="text">Hello Truc</div>
-                <p>I'm a funny guy</p>
+                <div class="text">Captain Kangaroo</div>
+                <p>No debate</p>
             </div>
         </div>
     </div>
