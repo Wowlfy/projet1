01d9d2812a714f16b05bbb55f647adfe90b3edb9
Wowlfy <maxime.giraudeau@hotmail.fr>
2021-03-24 15:44:32 +0100

    added forms on index and modifie form

--- form.php
+++ form.php
@@ -1,54 +1,9 @@
-<?php
-
-    
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = array_map('trim',$_POST);
-    
-    $errors = [];
-
-    if(empty($data['firstname'])) {
-        $errors[] = '⚠️ Le prénom est obligatoire';
-    }
-    $firstnameLenght = 20;
-    if (strlen($data['firstname']) > $firstnameLenght) {
-        $errors[] = '⚠️ Le prénom doit faire moins de 20 caractères';
-    }
-    if (empty($data['lastname'])) {
-        $errors[] = '⚠️ Le nom est obligatoire';
-    }
-    $lastnameLenght = 20;
-    if (strlen($data['lastname']) > $lastnameLenght) {
-        $errors[] = '⚠️ Le nom doit faire moins de 20 caractères';
-    }
-    if(empty($data['email'])) {
-        $errors[] = '⚠️ L\'Email est obligatoire';
-    }
-    if(filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-         $errors[] = '⚠️ Mauvais format d\'email';
-    }
-    if(empty($errors)) {
-        header('Location: index.php');
-    }
-}
-?>
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>john McClane</title>
-    <link rel="stylesheet" href="assets/form_style.css">
-</head>
-
-
 
 <section class=form id="form">
         <div class=head>
             <h2>CONTACT MOI</h2>
         </div>
-<form action="" method="POST" novalidate>
+<form action="index.php" method="POST" novalidate>
     <?php if (!empty($errors)) : ?>
         <ul class="error">
         <?php foreach($errors as $error) : ?>
@@ -69,6 +24,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <button>ENVOYER</button>
     </form>  
 </section>
-
-</html>
 
--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = array_map('trim',$_POST);
+    
+    $errors = [];
+
+    if(empty($data['firstname'])) {
+        $errors[] = '⚠️ Le prénom est obligatoire';
+    }
+    $firstnameLenght = 20;
+    if (strlen($data['firstname']) > $firstnameLenght) {
+        $errors[] = '⚠️ Le prénom doit faire moins de 20 caractères';
+    }
+    if (empty($data['lastname'])) {
+        $errors[] = '⚠️ Le nom est obligatoire';
+    }
+    $lastnameLenght = 20;
+    if (strlen($data['lastname']) > $lastnameLenght) {
+        $errors[] = '⚠️ Le nom doit faire moins de 20 caractères';
+    }
+    if(empty($data['email'])) {
+        $errors[] = '⚠️ L\'Email est obligatoire';
+    }
+    if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+         $errors[] = '⚠️ Mauvais format d\'email';
+    }
+    if(empty($errors)) {
+        header('Location: index.php');
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +38,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="assets/main.css">
     <link rel="stylesheet" href="assets/navbar_style.css">
+    <link rel="stylesheet" href="assets/form_style.css">
     <title>John McClane</title>
 </head>
 
